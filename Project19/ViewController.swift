d3c4d6c8fa7248d52fc33535ff19123ca695ef6a
Keith Crooc <keith.crooc@gmail.com>
2021-11-09 20:01:35 -0500

    Initial Commit

---
+++ Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Keith Crooc on 2021-11-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
